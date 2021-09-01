3af2a8ccdb569c5cb21c565273858cf07f189e24
Remy Piazza <remy.piazza.etu@univ-lille.fr>
2021-09-02 00:36:40 +0200

    Update etat.java

--- src/etat.java
+++ src/etat.java
@@ -1,4 +1,11 @@
-
 public enum etat {
+	COMBAT("Quelle attaque voulez-vous faire?"),OBJET("Quel objet voulez-vous utilisez?"), CHANGESLIME("Quelle slime voulez-vous?"), MENUCHOIX("Effectuer une action");
 
+	public String msg = "";
+	etat(String msg) {
+		this.msg = msg;
+	}
+	public String getMsg() {
+		return msg;
+	}	
 }
