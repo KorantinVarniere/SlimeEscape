8f2d1125af0017831408ac1fbdd41c24fdd89bcc
Emma Salot <emma.salot.etu@univ-lille.fr>
2021-09-02 11:28:16 +0200

    modify Slime / Attacks

---
+++ src/slimescape/NormalAttacks.java
@@ -0,0 +1,14 @@
+package slimescape;
+
+public enum NormalAttacks {
+	CRUSH("Crush !",50),CHARGE("charge !",40),THROW("throw !",20);
+	
+	String msg;
+	int damage;
+	
+	private NormalAttacks(String msg, int damage) {
+		this.msg = msg;
+		this.damage = damage;
+	}
+	
+}
--- src/slimescape/Slime.java
+++ src/slimescape/Slime.java
@@ -1,23 +1,115 @@
 package slimescape;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 public class Slime {
 	private int hp;
 	private int hpMax;
+	private int level;
 	static ArrayList Slimes;
-	Attacks[] tab = new Attacks[3];
+	private int xp;
+	private int xpMax;
+	private boolean isDefended;
+	private NormalAttacks nAttacks;
+	private MagicalAttacks mAttacks;
 	private Types types ;
 	
 
-	public Slime(int hp,  Attacks[] tab, Types types) {
+	public Slime() {
 		super();
-		this.hp = hp;
+		this.types = giveType();
+		this.hp = 100;
 		hpMax = hp;
-		this.tab = tab;
-		this.types = types;
+		this.level = 1;
+		this.xpMax = level;
+		this.xp = 0;
+		this.nAttacks = giveNormalAttack(); 
+		this.mAttacks = giveMagicalAttack();
 	}
 
+	private Types giveType() {
+		Random rand1 = new Random();
+		int i = rand1.nextInt(4);
+		if (i == 1) {
+			return Types.FIRE;
+		}
+		if (i==2) {
+			return Types.AIR;
+		}
+		if (i==3) {
+		return Types.EARTH ;
+	}else 
+		return Types.WATER;
+	}
+	
+	private NormalAttacks giveNormalAttack () {
+		Random rand = new Random();
+		int i = rand.nextInt(4);
+		if (i == 1) {
+			return NormalAttacks.CHARGE;
+		}
+		if (i==2) {
+			return NormalAttacks.CRUSH;
+		}else
+		return NormalAttacks.THROW;
+	}
+	
+	private MagicalAttacks giveMagicalAttack () {
+		Random rand = new Random();
+		int i = rand.nextInt(4);
+		if (this.types == Types.FIRE){
+			if (i == 1) {
+				return MagicalAttacks.A;
+			}
+			if (i==2) {
+				return MagicalAttacks.B;
+			}else
+			return MagicalAttacks.C ;
+		}
+		if (this.types == Types.WATER){
+			if (i == 1) {
+				return MagicalAttacks.D;
+			}
+			if (i==2) {
+				return MagicalAttacks.E;
+			}else
+			return MagicalAttacks.F ;
+		}
+		if (this.types == Types.AIR){
+			if (i == 1) {
+				return MagicalAttacks.G;
+			}
+			if (i==2) {
+				return MagicalAttacks.H;
+			}else
+			return MagicalAttacks.I ;
+		}
+		if (this.types == Types.EARTH){
+			if (i == 1) {
+				return MagicalAttacks.J;
+			}
+			if (i==2) {
+				return MagicalAttacks.K;
+			}else
+			return MagicalAttacks.L ;
+		}
+		return MagicalAttacks.A;
+	}
+	
+	private void evolve () {
+		if (xpMax == xp) {
+			if (level != 10) {
+				level = level +1;
+				hpMax = hp+10;
+				hp = hpMax;
+				xp = 0;
+				xpMax = level;
+			}
+		}
+	}
+	
 	public int getHpMax() {
 		return hpMax;
 	}
@@ -36,6 +128,37 @@ public class Slime {
 
 	public boolean isalive() {
 		return hp >0;
+	}
+
+	public Types getTypes() {
+		return types;
+	}
+
+	public void setTypes(Types types) {
+		this.types = types;
+	}
+	
+	public int getXp() {
+		return xp;
+	}
+
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
+
+	@Override
+	public String toString() {
+		return "Slime [hp=" + hp + ", hpMax=" + hpMax + ", level=" + level + ", xp=" + xp + ", xpMax=" + xpMax
+				+ ", isDefended=" + isDefended + ", nAttacks=" + nAttacks + ", mAttacks=" + mAttacks + ", types="
+				+ types + "]";
+	}
+
+	public static void main(String[] args) {
+		Slime s1 = new Slime();
+		System.out.println(s1.toString());
+		s1.setXp(1);
+		s1.evolve();
+		System.out.println(s1.toString());
 	}
 }
 
