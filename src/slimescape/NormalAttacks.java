9c65352baceb50512ce5b1210841c4438349b518
Emma Salot <emma.salot.etu@univ-lille.fr>
2021-09-02 16:34:10 +0200

    namenormalattack

--- src/slimescape/NormalAttacks.java
+++ src/slimescape/NormalAttacks.java
@@ -1,7 +1,7 @@
 package slimescape;
 
 public enum NormalAttacks {
-	CRUSH("Crush !",50),CHARGE("charge !",40),THROW("throw !",20);
+	CRUSH("Crush",50),CHARGE("Charge",40),THROW("Throw",20);
 	
 	String msg;
 	int damage;
