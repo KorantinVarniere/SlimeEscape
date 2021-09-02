93186b67c6962c2f8c4b9b8a3aa74d2fe0e830cd
Victor Mougel <victor.mougel.etu@univ-lille.fr>
2021-09-02 16:25:31 +0200

    Update Player.java

--- src/slimescape/Player.java
+++ src/slimescape/Player.java
@@ -1,27 +1,45 @@
 package slimescape;
+
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Player {
 
 
 	private List<Slime> playerSlimes;
-	/*private List<Item> items;
-	 */
-	public Player(List<Slime> slimes){
-		playerSlimes=slimes;
-		//items=new ArrayList<Item>;
+	private List<Item> itemsHeal;
+	private List<Item> itemsXp;
+	 
+	public Player(){
+		playerSlimes=new ArrayList<Slime>();
+		itemsHeal=new ArrayList<Item>();
+		itemsXp=new ArrayList<Item>();
+		playerSlimes.add(new Slime());
+		itemsHeal.add(new HealPotion());
+		itemsHeal.add(new HealPotion());
+		itemsHeal.add(new HealPotion());
+		itemsXp.add(new XpPotion());
+		itemsXp.add(new XpPotion());
+		itemsXp.add(new XpPotion());
 	}
 	
-	/*
-		public void addItem(Item item){
-			items.add(Item);
-		}
+	public void addHeal(){
+		itemsHeal.add(new HealPotion());
+	}
 
-		public void removeItem(Item item){
-			items.remove(item);
-		}
-	*/
+	public void removeHeal(){
+		itemsHeal.remove(itemsHeal.get(0));
+	}
+	
+	public void addXp(){
+		itemsXp.add(new XpPotion());
+	}
+
+	public void removeXp(){
+		itemsXp.remove(itemsXp.get(0));
+	}
 	
 	public void addSlime(Slime slime){
 		playerSlimes.add(slime);
@@ -31,23 +49,40 @@ public class Player {
 		playerSlimes.remove(slime);
 	}
 
-	public void changeSlime(Slime ancienSlime,Slime nouveauSlime){
-		removeSlime(ancienSlime);
-		addSlime(nouveauSlime);
+	public void usePotion (Slime slime){
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Quel type potion voulez vous utiliser ? 1-Heal 2-Xp ");
+		int choix;
+		
+		while(true) {
+			try{
+				choix=scanner.nextInt();
+				break;
+			}catch(NoSuchElementException|IllegalStateException e) {
+				System.out.println("Invalid input, select 1 for heal potion or 2 for Xp potion");
+			}
+		}
+		
+		if(choix==1) {
+			if(!itemsHeal.isEmpty()) itemsHeal.get(0).UseItem(slime);
+		}else {
+			if(!itemsXp.isEmpty()) itemsXp.get(0).UseItem(slime);
+		}
+		
+		scanner.close(); //peut créer des erreurs
 	}
-
-	
-	/*public void heal(Slime slime, Item potion){
-		slime.setHp(slime.getHp()+potion.getHeal());
-	}*/
 
 	public void damage(Slime slime,int degats){
 		slime.setHp(slime.getHp()-degats);
 	}
+	
+	//Pour les test, 1 pour heal et 2 pour xp
+	public Item getItem(int item) {
+		return item==1 ? new HealPotion() : new XpPotion();
+	}
 
-	/*public int[] getPosition(){
-		
-	}*/
 }
+
 
 
